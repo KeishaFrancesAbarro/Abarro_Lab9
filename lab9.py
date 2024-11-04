a22bed25eb4822df5fb1f9636c6d84450de745ba
KeishaFrancesAbarro <keisha110705@gmail.com>
2024-11-04 20:09:20 +0800

    PatternLab9.py

--- lab9.py
+++ lab9.py
@@ -1,9 +1,9 @@
 rows = int(input("Enter number of rows: "))
 
-current_number = 1
+current_num = 1
 
 for number in range(1, rows + 1):
     for j in range(number):
-        print(current_number, end=" ")
-        current_number += 1
+        print(current_num, end=" ")
+        current_num += 1
     print() 
